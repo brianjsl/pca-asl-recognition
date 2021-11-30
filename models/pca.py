8dff8604ac6c21e9193d0d2d24fe8a8f5b97832e
Brian Lee <brianjslmit@gmail.com>
2021-11-30 18:03:14 -0500

    broken PCA

--- models/pca.py
+++ models/pca.py
@@ -1,13 +1,53 @@
 import numpy as np
 import os
 import torch, torchvision
-from sklearn import cross_validation as crossval
-from sklearn.decomposition import RandomizedPCA
-from sklearn.model_selection import GridSearchCV
+from sklearn.decomposition import PCA
 from sklearn.svm import SVC
+from torchvision import transforms
+from matplotlib import pyplot as plt
 
 import sys
 sys.path.append("../")
 from data_loader.data_loader import get_datasets
 from data_loader.transforms import Inversion, NormalNoise, Rotate
 from constants import DATA_LABELS
+
+# gpu support
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+#pca transforms
+pca_transforms = {
+    "base": transforms.Resize((32, 32)),
+    # "inversion": [Inversion(), transforms.Resize((32, 32))],
+    "normal noise": [NormalNoise(), transforms.Resize((32, 32))],
+    "rotate": [Rotate(), transforms.Resize((32, 32))],
+}
+transform_names = sorted(pca_transforms.keys())
+
+#plots the images
+def plot_images(images, titles, h, w, n_row=3, n_col=4):
+    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
+        plt.title(titles[i], size=12)
+        plt.xticks(())
+        plt.yticks(())
+
+
+# datasets
+all_datasets = get_datasets(os.getcwd() + "/../data", [2800, 200], pca_transforms)
+train_dataset, test_dataset = all_datasets["base"]
+augmented_datasets = [all_datasets[augmentation][1] for augmentation in transform_names[1:]]
+
+#principal component analysis
+pca = PCA(n_components = 100)
+pca.fit(train_dataset)
+X = pca.transform(train_dataset)
+
+#eigenfingers
+eigenfingers = pca.components_.reshape((100,200,200))
+titles = ["eigenfingers %i" % i for i in range(len(eigenfingers))]
+plot_images(eigenfingers, titles, 200, 200)
+
