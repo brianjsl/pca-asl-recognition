2490ff09dcf40bd9648097be2b94fc822844402d
Lilian Luong <lilianluong64@gmail.com>
2021-12-01 16:00:23 -0500

    testing params

--- run_things.py
+++ run_things.py
@@ -1,10 +1,12 @@
 # Imports
 import os
 import sys
+import time
 
 sys.path.append("../")
 
 import joblib
+import pickle as pkl
 from torch.utils.data import DataLoader
 
 from data_loader.data_loader import get_datasets
@@ -30,15 +32,24 @@ data_config = {
 }
 resized_data_config = add_resize_to_config(data_config, (32, 32))  # add resize to 32x32
 
-# Get datasets
-all_datasets = get_datasets(os.path.join(os.getcwd(), "data"), [2000, 500], resized_data_config)
-train_dataset, test_dataset = all_datasets["base"]
-print("Got datasets, flattening...")
+load_data_from_pickle = False
+if load_data_from_pickle:
+    with open("models/saved_models/data_matrices.pkl", "rb") as f:
+        train_image_matrix, train_label_matrix, test_image_matrix, test_label_matrix = pkl.load(f)
+else:
+    # Get datasets
+    all_datasets = get_datasets(os.path.join(os.getcwd(), "data"), [2000, 500], resized_data_config)
+    train_dataset, test_dataset = all_datasets["base"]
+    print("Got datasets, flattening...")
+    t0 = time.time()
 
-# Flatten datasets
-train_image_matrix, train_label_matrix = dataset_to_matrices(train_dataset, flatten=True, shuffle=True)
-test_image_matrix, test_label_matrix = dataset_to_matrices(test_dataset, flatten=True, shuffle=True)
-print(train_image_matrix.shape)
+    # Flatten datasets
+    train_image_matrix, train_label_matrix = dataset_to_matrices(train_dataset, flatten=True, shuffle=True)
+    test_image_matrix, test_label_matrix = dataset_to_matrices(test_dataset, flatten=True, shuffle=True)
+
+    with open("models/saved_models/data_matrices.pkl", "wb") as f:
+        pkl.dump((train_image_matrix, train_label_matrix, test_image_matrix, test_label_matrix), f)
+    print(f"Got datasets in {time.time() - t0:.2f} seconds, shape of image matrix is {train_image_matrix.shape}")
 
 # PCA
 print("Fitting PCA...")
@@ -47,12 +58,13 @@ pca_model = fit_pca(train_image_matrix, num_components=100, verbose=1)
 reduced_train_matrix = pca_model.transform(train_image_matrix)
 reduced_test_matrix = pca_model.transform(test_image_matrix)
 
-
-# SVM
-print("Fitting SVM...")
-# svm_model = joblib.load("models/saved_models/svm_test.rbf")
-svm_model = fit_svm(reduced_train_matrix, train_label_matrix, gamma=0.5, C=0.09, verbose=2)
-joblib.dump(svm_model, "models/saved_models/svm_test.rbf")
-print("Test acc:", svm_model.score(reduced_test_matrix, test_label_matrix))
+for C in (0.1, 0.2, 0.3):
+    gamma = 'scale'
+    # SVM
+    print("Fitting SVM...")
+    # svm_model = joblib.load("models/saved_models/svm_test.rbf")
+    svm_model = fit_svm(reduced_train_matrix, train_label_matrix, gamma=gamma, C=C, verbose=1)
+    joblib.dump(svm_model, f"models/saved_models/svm_test_{gamma}_{round(C*10)}.rbf")
+    print("Test acc:", svm_model.score(reduced_test_matrix, test_label_matrix))
 
 # TODO: i haven't tested fit_mlp or mlp_accuracy at all but they should work in place of lines 41, 42
